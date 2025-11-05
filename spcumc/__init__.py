7e3755ac5e599382aa98ff69282335872b924f78
HwanHeo <gjghks950@naver.com>
2025-11-05 09:23:52 +0000

    package update

--- spcumc/__init__.py
+++ spcumc/__init__.py
@@ -1,4 +1,16 @@
-from ._backend import sparse_marching_cubes, sparse_marching_cubes_from_points
+from ._backend import (
+    sparse_marching_cubes,
+    sparse_marching_cubes_from_points,
+    sparse_dual_marching_cubes,
+    sparse_dual_marching_cubes_from_points,
+    decimate_mesh
+)
 
-__all__ = ['sparse_marching_cubes', 'sparse_marching_cubes_from_points']
+__all__ = [
+    'sparse_marching_cubes',
+    'sparse_marching_cubes_from_points',
+    'sparse_dual_marching_cubes',
+    'sparse_dual_marching_cubes_from_points',
+    'decimate_mesh'
+]
 __version__ = '0.1.0'
